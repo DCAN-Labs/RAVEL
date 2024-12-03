08ff04fe52b795193089b79050501c245042d0bb
Paul Reiners <reine097@umn.edu>
2024-12-03 10:51:41 -0600

    Create control region of CSF.

--- R/dcan/create_ravel_control_region.R
+++ R/dcan/create_ravel_control_region.R
@@ -1,4 +1,4 @@
 source("./R/utils.R")
-dir   <- file.path(find.package("RAVELData"), "extdata")
+dir   <- file.path("/users/9/reine097/data/fairview-ag/anonymized/skull_stripped_anonymized_sample/")
 masks <- list.files(dir, full.names=TRUE, pattern="*mask*.nii*")
 intersect_mask  <- maskIntersect(masks, output.file=tempfile())
