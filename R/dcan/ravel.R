786b9a378ff732e7d1f16edc696fc6b298987bfb
Paul Reiners <reine097@umn.edu>
2024-12-31 09:06:53 -0600

    Error Handling:
    Wrap the normalizeRAVEL function call in tryCatch() to gracefully handle any errors and identify problematic files.

--- R/dcan/ravel.R
+++ R/dcan/ravel.R
@@ -8,9 +8,8 @@ input.files  <- list.files(dir, full.names=TRUE)
 brain.mask <- "/users/9/reine097/mni_icbm152_t1_tal_nlin_sym_09a_mask_int16.nii.gz" 
 # Confirm the input arguments match the expected format and data types for the normalizeRAVEL function.
 control.mask <- "/users/9/reine097/data/fairview-ag/anonymized/6_control_mask/control_mask_08.nii.gz"
-normalizeRAVEL(input.files,
-               brain.mask = brain.mask,
-               control.mask = control.mask,
-               k=1, 
-               returnMatrix=TRUE,
-               WhiteStripe=FALSE)
+tryCatch({
+  normalizeRAVEL(input.files, brain.mask = brain.mask, control.mask = control.mask, k=1, returnMatrix=TRUE, WhiteStripe=FALSE)
+}, error = function(e) {
+  message("Error: ", e)
+})
