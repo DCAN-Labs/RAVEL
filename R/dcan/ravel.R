a558d02c9bd065d24ab9bcb1d55bfe920eb448df
Paul Reiners <reine097@umn.edu>
2024-12-31 09:03:43 -0600

    Use an int-valued mask.

--- R/dcan/ravel.R
+++ R/dcan/ravel.R
@@ -5,7 +5,8 @@ source("./normalizeRAVEL.R")
 
 dir = "/users/9/reine097/data/fairview-ag/anonymized/4_processed/"
 input.files  <- list.files(dir, full.names=TRUE)
-brain.mask <- "/users/9/reine097/mni_icbm152_t1_tal_nlin_sym_09a_mask_float32.nii.gz" 
+brain.mask <- "/users/9/reine097/mni_icbm152_t1_tal_nlin_sym_09a_mask_int16.nii.gz" 
+# Confirm the input arguments match the expected format and data types for the normalizeRAVEL function.
 control.mask <- "/users/9/reine097/data/fairview-ag/anonymized/6_control_mask/control_mask_08.nii.gz"
 normalizeRAVEL(input.files,
                brain.mask = brain.mask,
